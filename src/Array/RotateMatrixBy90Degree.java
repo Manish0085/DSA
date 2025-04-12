57e9ca8700f0d4698b8d4b4404d30be4168fe8c4
Manish Kummar <manishk57107@gmail.com>
2025-04-12 17:03:18 +0530

    Rotate matrix by 90 Degree

---
+++ src/Array/RotateMatrixBy90Degree.java
@@ -0,0 +1,40 @@
+package Array;
+
+public class RotateMatrixBy90Degree {
+
+    public void bruteForce(int[][] matrix){
+        int n = matrix.length;
+        int[][] result = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                result[j][n-i-1] = matrix[i][j];
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = result[i][j];
+            }
+        }
+    }
+    
+    public void optimal(int[][] matrix){
+        int n = matrix.length;
+
+        for (int i = 0; i < n-1; i++) {
+            for (int j = i+1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n/2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n-j-1];
+                matrix[i][n-j-1] = temp;
+            }
+        }
+    }
+}
