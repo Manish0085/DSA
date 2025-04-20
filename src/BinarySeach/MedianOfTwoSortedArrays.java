f4d8cc02510e9b61e3f26235954df9bd0aae9b07
Manish Kummar <manishk57107@gmail.com>
2025-04-20 09:59:02 +0530

    Median of two sorted array Optimised solution

--- src/BinarySeach/MedianOfTwoSortedArrays.java
+++ src/BinarySeach/MedianOfTwoSortedArrays.java
@@ -74,4 +74,37 @@ public class MedianOfTwoSortedArrays {
             return idx2el;
         return (double) ((double)(idx1el+idx2el))/2.0;
     }
+
+    public double optimal(int[] num1, int[] num2){
+        int n1 = num1.length;
+        int n2 = num2.length;
+        int n = n1 + n2;
+        if(n1 > n2){
+            return better(num2, num1);
+        }
+        int low = 0;
+        int high = n1;
+        int left = (n1 + n2 + 1)/2;
+
+        while (low <= high){
+            int mid1 = (low + high)/2;
+            int mid2 = left - mid1;
+            int l1 = (mid1 -1 >= 0)? num1[mid1-1] :Integer.MIN_VALUE;
+            int l2 = (mid2 -1 >= 0)? num2[mid2-1] :Integer.MIN_VALUE;
+            int r1 = (mid1 < n1)? num1[mid1] :Integer.MAX_VALUE;
+            int r2 = (mid2 < n2)? num2[mid2] :Integer.MAX_VALUE;
+
+            if (l1 <= r2 && l2 <= r1){
+                if (n % 2 == 1)
+                    return Math.max(l1, l2);
+                else
+                    return (double) (Math.max(l1, l2)+Math.min(r1, r2))/2.0;
+            } else if (l1 > r2) {
+                high = mid1 - 1;
+            }
+            else
+                low = mid1 + 1;
+        }
+        return 0;
+    }
 }
