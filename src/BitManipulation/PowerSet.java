dd45e9e6bef75341f0e4d6b1864da1c66e2d8d6a
Manish Kummar <manishk57107@gmail.com>
2025-05-06 00:06:51 +0530

    Power set

--- src/BitManipulation/PowerSet.java
+++ src/BitManipulation/PowerSet.java
@@ -1,4 +1,21 @@
 package BitManipulation;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PowerSet {
+
+    public static List<List<Integer>> findPowerSet(int[] arr){
+        int subsets = 1 << arr.length;
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int num = 0; num < subsets-1; num++) {
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < arr.length-1; i++) {
+                if ((num & (1 << i)) != 0)
+                    list.add(arr[i]);
+            }
+            ans.add(list);
+        }
+        return ans;
+    }
 }
