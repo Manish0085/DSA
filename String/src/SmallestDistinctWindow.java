30c508bc83f5bb3359b2496211f6ee7e0662b12c
Manish0085 <manishk57107@gmail.com>
2025-08-01 16:48:58 +0530

    Smallest Distinct Window

--- String/src/SmallestDistinctWindow.java
+++ String/src/SmallestDistinctWindow.java
@@ -1,2 +1,42 @@
+import java.util.Arrays;
+
 public class SmallestDistinctWindow {
+
+    public int findSubString(String s) {
+        int[] count = new int[256];
+        Arrays.fill(count, 0);
+        int diff = 0;
+        for (int i = 0; i < count.length; i++) {
+            if(count[s.charAt(i)] == 0){
+                diff++;
+            }
+            count[s.charAt(i)]++;
+        }
+
+        Arrays.fill(count, 0);
+        int first = 0;
+        int second = 0;
+        int length = s.length();
+        while (second < s.length()){
+            while (diff != 0 && second < s.length()){
+                if (count[s.charAt(second)] == 0){
+                    diff--;
+                }
+                count[s.charAt(second)]++;
+                second++;
+            }
+            length = Math.min(length, second-first);
+
+            while (diff != 1){
+                length = Math.min(length, second-first);
+                count[s.charAt(first)]--;
+                if (count[s.charAt(first)] == 0){
+                    diff++;
+                }
+                first++;
+            }
+        }
+
+        return length;
+    }
 }
