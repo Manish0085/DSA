4b205f889be5a78542dea949e0df9b1540191b1a
Manish0085 <manishk57107@gmail.com>
2025-08-02 11:25:10 +0530

    Minimum Size Subarray Sum BruteForce and Better approach

---
+++ Array/src/SlidingWindow/MinimumSizeSubarraySum.java
@@ -0,0 +1,39 @@
+package SlidingWindow;
+
+public class MinimumSizeSubarraySum {
+
+    public int minSubArrayLen1(int target, int[] nums) {
+
+        int length = nums.length+1;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+                int sum = 0;
+                for (int k = i; k <= j; k++) {
+                    sum += nums[k];
+                }
+                if (sum >= target)
+                    length = Math.min(length, j-i+1);
+            }
+        }
+        return (length == nums.length+1)? 0: length;
+
+    }
+
+
+    public int minSubArrayLen2(int target, int[] nums) {
+
+        int length = nums.length+1;
+        for (int i = 0; i < nums.length; i++) {
+            int sum = 0;
+            for (int j = i; j < nums.length; j++) {
+                sum += nums[j];
+                if (sum >= target)
+                    length = Math.min(length, j-i+1);
+            }
+        }
+        return (length == nums.length+1)? 0: length;
+
+    }
+
+
+}
