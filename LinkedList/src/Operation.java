a212bbf38d7ba2836d2151d57bc1e13d903902c4
Manish0085 <manishk57107@gmail.com>
2025-08-05 09:30:30 +0530

    Linked List changes commit

--- LinkedList/src/Operation.java
+++ LinkedList/src/Operation.java
@@ -30,4 +30,33 @@ public class Operation {
         return head;
     }
 
+
+    public Node insertAtGivenPos(Node head, int value, int pos) {
+        Node newNode = new Node(value);
+
+        if (pos == 0) { // Insertion at head
+            newNode.next = head;
+            return newNode;
+        }
+
+        Node temp = head;
+        for (int i = 0; i < pos - 1; i++) {
+            if (temp == null) {
+                System.out.println("Position out of bounds");
+                return head;
+            }
+            temp = temp.next;
+        }
+
+        if (temp == null) {
+            System.out.println("Position out of bounds");
+            return head;
+        }
+
+        newNode.next = temp.next;
+        temp.next = newNode;
+        return head;
+    }
+
+
 }
