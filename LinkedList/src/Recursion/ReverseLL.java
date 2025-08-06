910d3274eca472416e1ee220ea6f44a555bc6362
Manish0085 <manishk57107@gmail.com>
2025-08-06 09:04:12 +0530

    Rrvesre LL uisng recursion

---
+++ LinkedList/src/Recursion/ReverseLL.java
@@ -0,0 +1,14 @@
+package Recursion;
+
+public class ReverseLL {
+
+    public Node reverse(Node head, Node pre){
+        if(head == null || head.next == null)
+            return pre;
+
+        Node fut = head.next;
+        head.next = pre;
+        return reverse(fut, head);
+
+    }
+}
