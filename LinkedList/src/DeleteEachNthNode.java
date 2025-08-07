4de1349e5d08dd808569cf38686878dba3152176
Manish0085 <manishk57107@gmail.com>
2025-08-07 18:45:54 +0530

    Remove every kth node

---
+++ LinkedList/src/DeleteEachNthNode.java
@@ -0,0 +1,35 @@
+public class DeleteEachNthNode {
+
+    Node deleteK(Node head, int k) {
+        Node temp = head;
+        Node pre = null;
+        int count = 1;
+        while (temp != null){
+            if(count == k){
+                pre.next = temp.next;
+                temp = pre.next;
+                count = 1;
+            }else {
+                pre = temp;
+                temp = temp.next;
+                count++;
+            }
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        // Create first linked list: 1 -> 3 -> 5
+        Node head1 = new Node(10);
+        head1.next = new Node(9);
+//        head1.next.next = new Node(5);
+
+        // Create second linked list: 2 -> 4 -> 6 -> 8 -> 10
+        Node head2 = new Node(6);
+        head2.next = new Node(1);
+        head2.next.next = new Node(2);
+        head2.next.next.next = new Node(3);
+        head2.next.next.next.next = new Node(4);
+        head2.next.next.next.next.next = new Node(5);
+    }
+ }
