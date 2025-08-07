842020187c890d06ca7715a82c9dcb217617ed1c
Manish0085 <manishk57107@gmail.com>
2025-08-07 18:16:13 +0530

    Changes commit

--- LinkedList/src/RemoveNthNode.java
+++ LinkedList/src/RemoveNthNode.java
@@ -58,34 +58,5 @@ public class RemoveNthNode {
         return head;
     }
 
-    public static void printLL(Node head){
-        Node temp = head;
-        while (temp != null){
-            System.out.print(temp.data+" --> ");
-            temp = temp.next;
-        }
-        System.out.println("null");
-    }
-
-
-
-
-    public static void main(String[] args) {
-        // Create first linked list: 1 -> 3 -> 5
-        Node head1 = new Node(1);
-        head1.next = new Node(2);
-        head1.next.next = new Node(3);
-        head1.next.next.next = new Node(4);
-        head1.next.next.next.next = new Node(5);
-        head1.next.next.next.next.next = new Node(6);
-
-        RemoveNthNode rm = new RemoveNthNode();
-        rm.removeNthFromEnd(head1, 1);
-        printLL(head1);
-
-
-
-        System.out.println("\nRearranged Linked list:");
-    }
 
 }
