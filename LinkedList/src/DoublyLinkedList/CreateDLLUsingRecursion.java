550b7292a096dfccab30868e6ed9af4183d2d8d0
Manish0085 <manishk57107@gmail.com>
2025-08-08 12:56:34 +0530

    Add the Node in the DLL just after the given position assuming the 0-based indexing

--- LinkedList/src/DoublyLinkedList/CreateDLLUsingRecursion.java
+++ LinkedList/src/DoublyLinkedList/CreateDLLUsingRecursion.java
@@ -4,19 +4,19 @@ public class CreateDLLUsingRecursion {
 
     public Node createDLL(int[] arr, int idx, Node node){
 
-        if(idx == arr.length){
+        if(idx == -1){
             return null;
         }
         Node newNode = new Node(arr[idx]);
         newNode.prev = node;
-        newNode.next = createDLL(arr, ++idx, newNode);
+        newNode.next = createDLL(arr, --idx, newNode);
         return newNode;
     }
 
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
         CreateDLLUsingRecursion obj = new CreateDLLUsingRecursion();
-        Node head = obj.createDLL(arr, 0, null);
+        Node head = obj.createDLL(arr, arr.length-1, null);
         Node trav = head;
         while (trav != null){
             System.out.print(trav.data + " --> ");
---
+++ LinkedList/src/DoublyLinkedList/InsertTheNodeJustAfterNthPos.java
@@ -0,0 +1,30 @@
+package DoublyLinkedList;
+
+import java.lang.reflect.AnnotatedElement;
+
+public class InsertTheNodeJustAfterNthPos {
+
+    // Assuming that the indexing is 0-based
+    public Node insert(Node head, int p, int x){
+        if (head == null && p == 1){
+            head = new Node(x);
+            return head;
+        }
+
+        Node temp = head;
+        for (int i = 0; i < p; i++) {
+            temp = temp.next;
+        }
+
+        Node newNode = new Node(x);
+        Node fut = temp.next;
+        temp.next = newNode;
+        newNode.prev = temp;
+        if (fut != null){
+            fut.prev = newNode;
+            newNode.next = fut;
+        }
+
+        return head;
+    }
+}
