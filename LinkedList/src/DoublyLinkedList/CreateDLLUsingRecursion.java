75a2a665437c996f14b096900dbb5dbf19771a98
Manish0085 <manishk57107@gmail.com>
2025-08-08 12:38:32 +0530

    Creating DLL using recursion

---
+++ LinkedList/src/DoublyLinkedList/CreateDLLUsingRecursion.java
@@ -0,0 +1,28 @@
+package DoublyLinkedList;
+
+public class CreateDLLUsingRecursion {
+
+    public Node createDLL(int[] arr, int idx, Node node){
+
+        if(idx == arr.length){
+            return null;
+        }
+        Node newNode = new Node(arr[idx]);
+        newNode.prev = node;
+        newNode.next = createDLL(arr, ++idx, newNode);
+        return newNode;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        CreateDLLUsingRecursion obj = new CreateDLLUsingRecursion();
+        Node head = obj.createDLL(arr, 0, null);
+        Node trav = head;
+        while (trav != null){
+            System.out.print(trav.data + " --> ");
+            trav = trav.next;
+        }
+        System.out.println("null");
+    }
+
+}
