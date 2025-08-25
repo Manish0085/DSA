c1ee41743c9c69d7fad6115407851dac714a5401
Manish0085 <manishk57107@gmail.com>
2025-08-25 07:50:56 +0530

    Preorder traversal and BST using itreative approach

--- Binary Search Tree/src/PreorderTraversalAndBST.java
+++ Binary Search Tree/src/PreorderTraversalAndBST.java
@@ -1,9 +1,45 @@
+import java.util.Stack;
+
 public class PreorderTraversalAndBST {
+
+    public int canRepresentBST2(int arr[], int N){
+
+        Stack<Integer> lower = new Stack<>();
+        Stack<Integer> upper = new Stack<>();
+        lower.push(Integer.MIN_VALUE);
+        upper.push(Integer.MAX_VALUE);
+        int left = 0;
+        int right = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < lower.peek())
+                return 0;
+            while (arr[i] > upper.peek()){
+                upper.pop();
+                lower.pop();
+            }
+            left = lower.pop();
+            right = upper.pop();
+            // right
+            lower.push(arr[i]);
+            upper.push(right);
+
+            // left
+            lower.push(left);
+            upper.push(arr[i]);
+
+        }
+        return 1;
+    }
+
+
+
+
 
     // Recursive Approach
     int index = 0;
 
-    public int canRepresentBST(int arr[], int N){
+    public int canRepresentBST1(int arr[], int N){
         isBST(arr, Integer.MIN_VALUE, Integer.MAX_VALUE, N);
         return index == N? 1: 0;
     }
@@ -12,9 +48,6 @@ public class PreorderTraversalAndBST {
 
         if (index == N || arr[index] < lower || arr[upper] > upper)
             return;
-
-
-
         int value = arr[index++];
         isBST(arr, lower, value, N);
         isBST(arr, value, upper, N);
