71e0e122eb87af1c17f313d96e5d29d7217fbd7e
Manish0085 <manishk57107@gmail.com>
2025-08-27 22:14:35 +0530

    Lowest Common Ancestor

--- Binary Search Tree/src/LowestCommonAncestor.java
+++ Binary Search Tree/src/LowestCommonAncestor.java
@@ -2,13 +2,13 @@ public class LowestCommonAncestor {
 
 
     public Node LCA(Node root, int n1, int n2){
-        if (root === null)
+        if (root == null)
             return null;
 
         if(root.data > n1 && root.data > n2)
-            return LCA(root.right, n1, n2);
-        else if(root.data < n1 && root.data < n2)
             return LCA(root.left, n1, n2);
+        else if(root.data < n1 && root.data < n2)
+            return LCA(root.right, n1, n2);
         else
             return root;
     }
