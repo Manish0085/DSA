5dc6127ef0b8e60b2d5ece47e3ab2266d50fbfd7
Manish0085 <manishk57107@gmail.com>
2025-09-03 09:37:06 +0530

    Largest BST

---
+++ Binary Search Tree/src/LargestBST.java
@@ -0,0 +1,14 @@
+public class LargestBST {
+
+    boolean BST;
+    int size;
+    int max, min;
+
+    LargestBST(int data){
+        this.BST = true;
+        this.size = 1;
+        this.max = data;
+        this.min = data;
+    }
+
+}
