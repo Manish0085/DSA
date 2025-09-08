6871febfab9a68a2588c647f8e0d2486f9469f3c
Manish0085 <manishk57107@gmail.com>
2025-09-08 09:03:16 +0530

    Minimum cost of ropes

---
+++ Heap/src/MinimumCaostOfRopews.java
@@ -0,0 +1,18 @@
+import java.util.PriorityQueue;
+
+public class MinimumCaostOfRopews {
+
+    public int minCost(int[] arr){
+        PriorityQueue<Integer> priQue = new PriorityQueue<>();
+        for (int i = 0; i < arr.length; i++) {
+            priQue.add(arr[i]);
+        }
+
+        int cost = 0;
+        while (priQue.size() > 1){
+            cost += (priQue.remove() + priQue.remove());
+        }
+
+        return cost;
+    }
+}
