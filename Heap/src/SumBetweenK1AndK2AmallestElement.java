26dbf59597966c9e4a319fe38740d2d3c68c51e4
Manish0085 <manishk57107@gmail.com>
2025-09-09 09:24:47 +0530

    Sum of Element between K1 and K2 smallest

---
+++ Heap/src/SumBetweenK1AndK2AmallestElement.java
@@ -0,0 +1,37 @@
+import java.util.PriorityQueue;
+
+public class SumBetweenK1AndK2AmallestElement {
+
+
+    public int sum(int[] arr, int k1, int k2){
+        PriorityQueue<Integer> priQue1 = new PriorityQueue<>();
+        PriorityQueue<Integer> priQue2 = new PriorityQueue<>();
+        for (int i = 0; i < k1; i++) {
+            priQue1.add(arr[i]);
+        }
+        for (int i = 0; i < k2; i++) {
+            priQue2.add(arr[i]);
+        }
+
+        for (int i = k1; i < arr.length; i++) {
+            if (arr[i] < priQue1.peek()){
+                priQue1.remove();
+                priQue1.add(arr[i]);
+            }
+        }
+        for (int i = k2; i < arr.length; i++) {
+            if (arr[i] < priQue2.peek()){
+                priQue2.remove();
+                priQue2.add(arr[i]);
+            }
+        }
+
+        int sum1 = priQue1.stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+        int sum2 = priQue1.stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+        return sum2 - sum1;
+    }
+}
