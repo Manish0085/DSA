9ba49cc7a30f4755835fa952e651c2829c08ae5b
Manish0085 <manishk57107@gmail.com>
2025-09-10 08:52:53 +0530

    Median of stream

---
+++ Heap/src/FindMedian.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
+public class FindMedian {
+
+
+
+    public ArrayList<Double> getMedian(int[] arr){
+        ArrayList<Double> list = new ArrayList<>();
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b-a);
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+
+        for(int num: arr){
+            maxHeap.offer(num);
+
+
+            if(!minHeap.isEmpty() && minHeap.peek() < maxHeap.peek()){
+                minHeap.offer(maxHeap.poll());
+            }
+
+            if (minHeap.size() + 1 < maxHeap.size()){
+                minHeap.offer(maxHeap.poll());
+            } else  {
+                maxHeap.offer(maxHeap.poll());
+            }
+
+            if (maxHeap.size() == minHeap.size()){
+                double median = (maxHeap.peek() + minHeap.peek())/2;
+                list.add(median);
+            }
+            else {
+                list.add((double)maxHeap.poll());
+            }
+        }
+        return list;
+    }
+
+
+}
