285596e6977ba586056a3ad04b3bdce75d84f177
Manish0085 <manishk57107@gmail.com>
2025-10-04 13:07:48 +0530

    Push changes

--- Tree/src/HeightOfBST.java
+++ Tree/src/HeightOfBST.java
@@ -1,12 +1,40 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class HeightOfBST {
 
-    private int height = 0;
+    private int height = -1;
 
     public int heightRecursively(Node root){
         if (root == null)
             return height;
+        int left = heightRecursively(root.left);
+        int right = heightRecursively(root.right);
 
-        int left = Math.max()
+        return 1 + Math.max(left, right);
+    }
+
+
+
+    public int heightIteratively(Node root){
+        int h = -1;
+        if (root == null)
+            return h;
+
+        Queue<Node> que = new LinkedList<>();
+        que.add(root);
+        while (!que.isEmpty()){
+            int n = que.size();
+            for (int i = 0; i < n; i++) {
+                Node temp = que.poll();
+                if (temp.left != null)
+                    que.add(temp.left);
+                if (temp.right != null)
+                    que.add(temp.right);
+            }
+            h++;
+        }
+        return h;
     }
 
 }
