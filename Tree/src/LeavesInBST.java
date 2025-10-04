3fcb7f9bb956224311fd0eff4dffe5e571ce85a8
Manish0085 <manishk57107@gmail.com>
2025-10-04 12:38:27 +0530

    Count leave

---
+++ Tree/src/LeavesInBST.java
@@ -0,0 +1,41 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LeavesInBST {
+
+    private int count = 0;
+
+    public int countRecursively(Node root){
+        if (root == null){
+            return this.count;
+        }
+
+        if (root.left == null && root.right != null)
+            this.count++;
+
+        countRecursively(root.left);
+        countRecursively(root.right);
+        return this.count;
+    }
+
+    public int countIteratively(Node root){
+        int countLeave = 0;
+        if (root == null)
+            return countLeave;
+
+        Queue<Node> que = new LinkedList<>();
+        que.add(root);
+        while (!que.isEmpty()){
+            Node temp = que.poll();
+            if (temp.left == null && temp.right == null)
+                countLeave++;
+
+            if (temp.left != null)
+                que.add(temp.left);
+
+            if (temp.right != null)
+                que.add(temp.right);
+        }
+        return countLeave;
+    }
+}
