1d512ac7f6fe43c066717c7fd9b357f7216d9826
Manish0085 <manishk57107@gmail.com>
2025-12-04 13:04:57 +0530

    Graph using Adjacency List

--- Graph/src/AdjacencyList/DUG.java
+++ Graph/src/AdjacencyList/DUG.java
@@ -1,46 +1,44 @@
 package AdjacencyList;
 
-import javax.script.ScriptContext;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Scanner;
 
-public class UUG {
+public class DUG {
 
-    private LinkedList<Integer>[] adjList;
     private int vertices;
+    private LinkedList<Integer>[] adjList;
 
-    public UUG(int size){
-        this.adjList = new LinkedList[size];
+
+    public DUG(int size){
         this.vertices = size;
+        this.adjList = new LinkedList[size];
 
         for (int i = 0; i < size; i++) {
             adjList[i] = new LinkedList<>();
         }
     }
 
+
     public void addEdge(int u, int v){
         if (u >= 0 && v >= 0 && u < vertices && v < vertices){
             adjList[u].add(v);
-            adjList[v].add(u);
-        }
-        else {
+        } else {
             System.out.println("Invalid edge!");
         }
     }
 
     public void removeEdge(int u, int v){
-        if (u >= 0 && v >= 0 && u < vertices && v < vertices){
+        if (u >= 0  && v >= 0 && u < vertices && v < vertices){
             adjList[u].remove(v);
-            adjList[v].remove(u);
-        }
-        else {
-            System.out.println("Invalid edge!");
+        } else {
+            System.out.println("Invalid Edge!");
         }
     }
 
-    public void printGraph(){
+    public void  printGraph(){
         for (int i = 0; i < vertices; i++) {
-            System.out.print("Vertex " + i + " --> ");
+            System.out.print("vertex " + i + " --> ");
             for (Integer node: adjList[i]){
                 System.out.print(node + " ");
             }
@@ -52,19 +50,17 @@ public class UUG {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of vertices: ");
         int n = sc.nextInt();
-
-        UUG obj = new UUG(n);
+        DUG obj = new DUG(n);
 
         System.out.print("Enter the number of edges: ");
         int e = sc.nextInt();
 
-        System.out.println("Enter the vertex (u, v): ");
+        System.out.println("Enter the vertex (v, u) and weight");
         for (int i = 0; i < e; i++) {
             int u = sc.nextInt();
             int v = sc.nextInt();
             obj.addEdge(u, v);
         }
-
         obj.printGraph();
     }
 }
--- Graph/src/AdjacencyList/UWG.java
+++ Graph/src/AdjacencyList/UWG.java
@@ -1,39 +1,38 @@
 package AdjacencyList;
 
-import javax.script.ScriptContext;
+import java.util.Collections;
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.Scanner;
 
-public class UUG {
+public class UWG {
 
-    private LinkedList<Integer>[] adjList;
     private int vertices;
+    private LinkedList<Map<Integer, Integer>>[] adjList;
 
-    public UUG(int size){
-        this.adjList = new LinkedList[size];
+    public UWG(int size){
         this.vertices = size;
+        this.adjList = new LinkedList[size];
 
         for (int i = 0; i < size; i++) {
             adjList[i] = new LinkedList<>();
         }
     }
 
-    public void addEdge(int u, int v){
+    public void addEdge(int u, int v, int w){
         if (u >= 0 && v >= 0 && u < vertices && v < vertices){
-            adjList[u].add(v);
-            adjList[v].add(u);
-        }
-        else {
+            adjList[u].add(Collections.singletonMap(v, w));
+            adjList[v].add(Collections.singletonMap(u, w));
+        } else {
             System.out.println("Invalid edge!");
         }
     }
 
-    public void removeEdge(int u, int v){
+    public void removeList(int u, int v){
         if (u >= 0 && v >= 0 && u < vertices && v < vertices){
-            adjList[u].remove(v);
-            adjList[v].remove(u);
-        }
-        else {
+            adjList[u].removeIf(map -> map.containsKey(v));
+            adjList[v].removeIf(map -> map.containsKey(u));
+        } else {
             System.out.println("Invalid edge!");
         }
     }
@@ -41,30 +40,32 @@ public class UUG {
     public void printGraph(){
         for (int i = 0; i < vertices; i++) {
             System.out.print("Vertex " + i + " --> ");
-            for (Integer node: adjList[i]){
-                System.out.print(node + " ");
+            for (Map<Integer, Integer> map: adjList[i]){
+                for (Map.Entry<Integer, Integer> set: map.entrySet()){
+                    System.out.print("(" + set.getKey() + ", w=" + set.getValue() + ") ");
+                }
             }
             System.out.println();
         }
     }
 
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of vertices: ");
         int n = sc.nextInt();
-
-        UUG obj = new UUG(n);
+        UWG obj = new UWG(n);
 
         System.out.print("Enter the number of edges: ");
         int e = sc.nextInt();
 
-        System.out.println("Enter the vertex (u, v): ");
+        System.out.println("Enter the vertex (v, u) and weight");
         for (int i = 0; i < e; i++) {
             int u = sc.nextInt();
             int v = sc.nextInt();
-            obj.addEdge(u, v);
+            int w = sc.nextInt();
+            obj.addEdge(u, v, w);
         }
-
         obj.printGraph();
     }
 }
