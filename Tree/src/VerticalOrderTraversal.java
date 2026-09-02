5621e87ccbd4c41a01a062fe7c0b3755c9a45af3
Manish0085 <manishk57107@gmail.com>
2026-09-02 18:31:10 +0530

    Vertical Sum of Binary Tree

--- Tree/src/VerticalOrderTraversal.java
+++ Tree/src/VerticalOrderTraversal.java
@@ -11,6 +11,17 @@ class Triplet {
         this.level = level;
     }
 }
+
+class Pair {
+    TreeNode node;
+    int vertical;
+
+
+    public Pair(TreeNode node, int vertical) {
+        this.node = node;
+        this.vertical = vertical;
+    }
+}
 public class VerticalOrderTraversal {
 
     public List<List<Integer>> verticalTraversal(TreeNode root) {
@@ -52,5 +63,59 @@ public class VerticalOrderTraversal {
         }
 
         return ans;
+    }
+
+    public ArrayList<ArrayList<Integer>> verticalOrderTraversal(TreeNode root) {
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+
+        if(root == null)
+            return ans;
+
+        TreeMap<Integer, ArrayList> map = new TreeMap<>();
+        Queue<Pair> que = new LinkedList<>();
+        que.offer(new Pair(root, 0));
+        while (!que.isEmpty()) {
+            Pair p = que.poll();
+            TreeNode node = p.node;
+            int vertical = p.vertical;
+
+            if(!map.containsKey(vertical))
+                map.put(vertical, new ArrayList());
+            map.get(vertical).add(node.val);
+            if (node.left != null)
+                que.offer(new Pair(node, vertical-1));
+
+            if (node.right != null)
+                que.offer(new Pair(node, vertical+1));
+        }
+
+        for (ArrayList<Integer> list: map.values())
+            ans.add(list);
+
+        return ans;
+     }
+
+    public ArrayList<Integer> verticalSum(TreeNode root) {
+        if(root == null)
+            return new ArrayList<>();
+
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        Queue<Pair> que = new LinkedList<>();
+        que.offer(new Pair(root, 0));
+        while (!que.isEmpty()) {
+            Pair pair = que.poll();
+            TreeNode node = pair.node;
+            int vertical = pair.vertical;
+
+            map.put(vertical, map.getOrDefault(vertical, 0) + node.val);
+
+            if(node.left != null)
+                que.offer(new Pair(node.left, vertical-1));
+
+            if(node.right != null)
+                que.offer(new Pair(node.right, vertical+1));
+        }
+
+        return new ArrayList<>(map.values());
     }
 }
