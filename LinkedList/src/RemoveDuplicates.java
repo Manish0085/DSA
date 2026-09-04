53fca0ee1ba66f555f76ab06bcb8d23b558da468
Manish0085 <manishk57107@gmail.com>
2026-09-04 18:37:17 +0530

    Remove Duplicates from thr list

--- LinkedList/src/RemoveDuplicates.java
+++ LinkedList/src/RemoveDuplicates.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class RemoveDuplicates {
+public class RemoveDuplicates
+{
+
+    public ListNode deleteDuplicates(ListNode A) {
+        if (A == null || A.next == null)
+            return A;
+
+        ListNode curr = A;
+        ListNode dummy = new ListNode(-1);
+        ListNode temp = dummy;
+
+        while (curr != null && curr.next != null) {
+            if (curr.val != curr.next.val) {
+                dummy.next = curr;
+                dummy = dummy.next;
+            }
+            curr = curr.next;
+        }
+
+        dummy.next = curr;
+
+        return temp.next;
+    }
 }
