74e74811cb475596f392ab31d6e91fd7d85049ac
Manish0085 <manishk57107@gmail.com>
2026-09-15 17:44:01 +0530

    Floyd warshal's algorith

---
+++ Graph/src/FloydWarshal/Solution.java
@@ -0,0 +1,19 @@
+package FloydWarshal;
+
+public class Solution {
+
+    public void floydWarshall(int[][] dist) {
+        int n = dist.length;
+        int INF = 100000000;
+        for (int via = 0; via < n; via++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+
+                    if(dist[i][via] != INF && dist[via][j] != INF)
+                        dist[i][j] = Math.min(dist[i][j], dist[i][via] + dist[via][j]);
+                }
+            }
+        }
+
+    }
+}
